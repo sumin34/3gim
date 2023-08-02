18df4150788439a8ac3b59601462675951d42dd7
hi <hi@DESKTOP-QL4DVPO>
2023-08-02 16:16:25 +0900

    페이지 확인차 컨트롤러 만듬

---
+++ 3gim/Controllers/MemberController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3gim.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
