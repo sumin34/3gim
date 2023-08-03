74907cb4cfb5b02cc67b7e97da66a62b1bd0437d
sujin <hwqjkrqw@naver.com>
2023-08-03 15:05:05 +0900

    signup

---
+++ 3gim/Controllers/MemberController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3gim.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+    }
+}
