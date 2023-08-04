89796f7fa3e96134300ddee75212c4b469a4c489
taehyun <ttu510@naver.com>
2023-08-04 09:59:11 +0900

    상품등록 backend

--- 3gim/Controllers/ProductController.cs
+++ 3gim/Controllers/ProductController.cs
@@ -1,28 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _3gim.Data;
+using _3gim.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _3gim.Controllers
 {
     [Route("product")]
     public class ProductController : Controller
     {
+        private readonly _3gimDbContext _dbContext;
+        private readonly UserManager<_3gimMember> _userManager;
+
+
+        public ProductController(_3gimDbContext dbContext, UserManager<_3gimMember> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        [HttpGet("print")]
         public IActionResult Print()
         {
             return View();
         }
 
+        [HttpGet("Count")]
         public IActionResult Count()
         {
             return View();
         }
+
+        [HttpGet("Order")]
         public IActionResult Order()
         {
             return View();
         }
+
+        [HttpGet("Productquantity")]
         public IActionResult Productquantity()
         {
             return View();
         }
 
+        [HttpGet("productedit")]
         public IActionResult Productedit()
         {
             return View();
@@ -35,9 +56,13 @@ namespace _3gim.Controllers
         }
 
         [HttpPost("productregist")]
-        public IActionResult Productregist(string productname, int exp)
+        public IActionResult Productregist(Product product)
         {
-            return View();
+            _dbContext.Add(product);
+
+            _dbContext.SaveChanges();
+
+            return Redirect("/product/productregist");
         }
 
       
