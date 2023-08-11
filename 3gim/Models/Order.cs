89290c2542e9576613b4b3fd01c86385b3f978d8
taehyun <ttu510@naver.com>
2023-08-11 14:06:21 +0900

    db수정

---
+++ 3gim/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _3gim.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderNumber { get; set; } // 주문번호
+        public string Vendor { get; set; } // 거래처
+        public string ShippingAddress { get; set; }// 배송지
+        public int ProductID { get; set; }
+        public Product PID { get; set; }
+        public int OrderQuantity { get; set; } // 주문수량
+        public string Note { get; set; } // 비고
+    }
+}
---
+++ 3gim/Models/Warehousing.cs
@@ -0,0 +1,14 @@
+﻿namespace _3gim.Models
+{
+    public class Warehousing
+    {
+        public int Id { get; set; } // No.
+        public string Date { get; set; } // 날짜
+        public int LotNumber { get; set; } // 제조번호
+        public int Store { get; set; } // 입고 수량
+        public int Release { get; set; } // 출고 수       
+        public string Note { get; set; } // 기타
+        public int ProductID { get; set; }
+        public Product PID { get; set; }
+    }
+}
