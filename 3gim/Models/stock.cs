0b81d9cb30bef72990cbeaf09715678c674b5af0
taehyun <ttu510@naver.com>
2023-08-11 17:00:36 +0900

    재고현황 구현 완료

---
+++ 3gim/Models/stock.cs
@@ -0,0 +1,12 @@
+﻿namespace _3gim.Models
+{
+    public class stock
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int TotalStore { get; set; }
+        public int TotalRelease { get; set; }
+        public int CurrentQuantity { get; set; }
+
+    }
+}
