83a9e52534b10209adc26bdc8ff3cc7c8f024ff0
hi <hi@DESKTOP-QL4DVPO>
2023-08-14 16:22:59 +0900

    카메라허브 푸시

---
+++ 3gim/Hubs/CameraHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MySqlX.XDevAPI;
+
+namespace _3gim.Hubs
+{
+    public class CameraHub : Hub
+    {
+        public async Task BeltCamera(string message)
+        {
+            await Clients.All.SendAsync("ReceiveVideo", message);
+        }
+        public async Task BeltCamera2(string message)
+        {
+            await Clients.All.SendAsync("ReceiveVideo2", message);
+        }
+
+        public async Task AICamera(string message)
+        {
+            await Clients.All.SendAsync("ReceiveAI", message);
+        }
+    }
+}
